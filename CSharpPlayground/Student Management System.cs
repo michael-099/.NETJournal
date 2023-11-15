80e3ce45d35fbad4b313926d2824815b91ce15ab
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-11-15 11:54:15 +0300

    Student Management System

---
+++ CSharpPlayground/Student Management System.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Student{
+    class Student_Management_System{
+   
+   static void Main(string[] args) {
+    Console.WriteLine("Hello, world!");
+    }
+    
+}
+
+
+    
+}
+
