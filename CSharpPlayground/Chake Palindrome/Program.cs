48d527edac3b5165ebef17adf1afc3ce4bfec1b1
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-11-21 22:04:32 +0300

    Check Pal

---
+++ CSharpPlayground/Chake Palindrome/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+
+
+
+
+// Palindrome Check
+//Write a C# function that takes a string as input and checks whether it
+//is a palindrome or not. A palindrome is a word, phrase, number, or other
+//sequence of characters that reads the same forward and backward 
+//(ignoring spaces, punctuation, and capitalization).
+//[Optional]: Write test for your function
