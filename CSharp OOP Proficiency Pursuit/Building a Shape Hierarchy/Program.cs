4ee628aa83b9424efec9400d187b02cbceb1e822
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-11-22 10:57:37 +0300

    bluprint for the implementation

---
+++ CSharp OOP Proficiency Pursuit/Building a Shape Hierarchy/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace ShapeHierarchy;
+class shape
+{
+    string Name;
+
+
+    class Circle
+    {
+        double Radius;
+        CalculateArea()
+        {
+
+        }
+
+
+    }
+
+    class Rectangle
+    {
+        double Width;
+        double Height;
+        CalculateArea()
+        {
+
+        }
+
+
+    }
+
+    class Triangle
+    {
+        double Base;
+        double Height;
+        CalculateArea()
+        {
+
+        }
+
+
+
+    }
+
+    void PrintShapeArea(Shape shape)
+    {
+
+
+    }
+
+    static void Main(string[] args)
+    {
+
+
+    }
+
+
+}
