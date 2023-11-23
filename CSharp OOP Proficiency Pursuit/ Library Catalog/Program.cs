6232999966fe1ad072394190fac6c9e4c5c4dab1
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-11-23 16:54:01 +0300

    blue print

--- CSharp OOP Proficiency Pursuit/ Library Catalog/Program.cs
+++ CSharp OOP Proficiency Pursuit/ Library Catalog/Program.cs
@@ -1,51 +1,85 @@
 ﻿using System;
+using LibraryCatable;
 namespace LibraryCatable;
 class Library
 {
     string Name;
     string Address;
-    List<string> Books = new List<string>();
-    List<string> Media = new List<string>();
+    List<Book> Books = new List<Book>();
+    List<MediaItem> Media = new List<MediaItem>();
     void AddBook(Book book)
     {
+        Books.Add(book);
 
     }
     void RemoveBook(Book book)
     {
+        Books.Remove(book);
 
 
     }
 
     void AddMediaItem(MediaItem mediaItem)
     {
+        Media.Add(book);
     }
 
     void RemoveMediaItem(MediaItem mediaItem)
     {
-
+        Media.Remove(mediaItem);
     }
-    void PrintCatalog(){
+    public static void PrintCatalog()
+    {
+        Console.WriteLine("----CATALOG ----");
+        Console.WriteLine("Book Catalog");
+        for (int i=0 ;i<=Books.Length ;i++){
+            Console.WriteLine(Books[i].Name);
+        }
+         Console.WriteLine("Media Catalog");
+          for (int i=0 ;i<=Media.Length ;i++){
+            Console.WriteLine(Media[i].Name);
+        }
+
+
 
     }
 }
-class Book {
+class Book
+{
 
     string Title;
     string Author;
     string ISBN;
     int PublicationYear;
 
+    Book(string title, string author, string isbn, int puplicationYear)
+    {
+        Title = title;
+        Author = author;
+        ISBN = isbn;
+        PublicationYear = publicationYear;
+    }
+
 }
- class MediaItem{
+class MediaItem
+{
 
     string Title;
     string MediaType;
-    int Duration ;
+    int Duration;
+    MediaItem(string title, string mediaType, int duration;){
+    Title=title;
+    MediaType=mediaType;
+    Duration=duration;
 
- }
- class Program {
-    void Main(string[] args ){
+
+}
+}
+class Program
+{
+    void Main(string[] args)
+    {  Library 
         printCatalog();
 
     }
- }
+}
