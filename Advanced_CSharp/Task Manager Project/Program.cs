44a8211edc4a4eddfa5bcb6e3667c8a0dbfa9962
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-11-24 11:08:41 +0300

    TMP

--- Advanced_CSharp/Task Manager Project/Program.cs
+++ Advanced_CSharp/Task Manager Project/Program.cs
@@ -1,5 +1,49 @@
 ﻿using System;
 namespace deeper_CSharp;
-class Systems{
+public enum Tasks
+{
+    Personal,
+    Work,
+    Errands,
 
+}
+public enum Catagory
+{
+    Todo,
+    Urgent,
+    Important,
+
+
+}
+class Task
+{
+    List<Task> TaskManager = new List<Task>()
+    string Name;
+    Catagory Catagory;
+    string Description;
+
+    bool ISCompleted;
+    void Manager(Task task)
+    {
+        TaskManager.Add(task)
+
+    }
+    void PrintTask(){
+       for(each x in TaskManager){
+        Consolog.WriteLine($"Name:{x.name} Discription:{x.description} isompleted:{x.isCompleted} Catagory:{x.Catagory}")
+       }
+    }
+    Task(string Name,Catagory Catagory,string Description, bool ISCompleted){
+        this.Name=Name;
+        this.Catagory=Catagory;
+        this.Description=Description;
+        this.ISCompleted=ISCompleted;
+
+    }
+
+}
+class Program{
+    public static void Main(string[] args){
+        
+    }
 }
