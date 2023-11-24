915a78238bc4bb063c2942bb6ba260d38e4bf080
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-11-24 10:00:52 +0300

    advanced cs

---
+++ Advanced_CSharp/Task Manager Project/Program.cs
@@ -0,0 +1,5 @@
+﻿using System;
+namespace deeper_CSharp;
+class Systems{
+
+}
---
+++ CSharp OOP Proficiency Pursuit/Advanced_CSharp/Going Deeper into C#/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace deeper_CSharp;
+public enum Tasks
+{
+    Personal,
+    Work,
+    Errands,
+
+}
+public enum Catagory
+{
+
+}
+class Task
+{
+    string Name;
+    string Description;
+
+    bool ISCompleted;
+    void Manager(){
+        
+    } 
+
+}
