2b41a6fef68efaf836925b7e117f79bf24961e3c
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-11-24 11:33:46 +0300

    TMP

--- Advanced_CSharp/Task Manager Project/Program.cs
+++ Advanced_CSharp/Task Manager Project/Program.cs
@@ -1,49 +1,60 @@
 ﻿using System;
 namespace deeper_CSharp;
-public enum Tasks
+
+public enum Catagory
 {
     Personal,
     Work,
     Errands,
 
-}
-public enum Catagory
-{
-    Todo,
-    Urgent,
-    Important,
 
 
 }
 class Task
 {
-    List<Task> TaskManager = new List<Task>()
-    string Name;
-    Catagory Catagory;
-    string Description;
+    public static List<Task> TaskManager = new List<Task>();
+    string Name { get; set; }
+    Catagory Catagory { get; set; }
+    string Description { get; set; }
 
-    bool ISCompleted;
-    void Manager(Task task)
+    bool ISCompleted { get; set; }
+    public static void AddTask(Task task)
     {
-        TaskManager.Add(task)
+        TaskManager.Add(task);
 
     }
-    void PrintTask(){
-       for(each x in TaskManager){
-        Consolog.WriteLine($"Name:{x.name} Discription:{x.description} isompleted:{x.isCompleted} Catagory:{x.Catagory}")
-       }
+    public static void PrintTask()
+    {
+        foreach (var x in TaskManager)
+        {
+            Console.WriteLine($"Name:{x.Name} Discription:{x.Description} isompleted:{x.ISCompleted} Catagory:{x.Catagory}");
+        }
     }
-    Task(string Name,Catagory Catagory,string Description, bool ISCompleted){
-        this.Name=Name;
-        this.Catagory=Catagory;
-        this.Description=Description;
-        this.ISCompleted=ISCompleted;
+    public Task(string Name, Catagory Catagory, string Description, bool ISCompleted)
+    {
+        this.Name = Name;
+        this.Catagory = Catagory;
+        this.Description = Description;
+        this.ISCompleted = ISCompleted;
 
     }
 
 }
-class Program{
-    public static void Main(string[] args){
-        
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Task task1 = new Task("Complete Homework Assignment", Catagory.Work, "Finish the homework assignment for mathematics and submit it by 5 PM.", false);
+        Task.AddTask(task1);
+        Task task2 = new Task("Grocery Shopping", Catagory.Personal, "Buy groceries for the week, including fruits, vegetables, and essential household items.", false);
+        Task.AddTask(task2);
+        Task task3 = new Task("Write Blog Post", Catagory.Work, " Create a new blog post on the latest technology trends and innovations. Include relevant examples and insights.", false);
+        Task.AddTask(task3);
+        Task task4 = new Task("Gym Workout", Catagory.Personal, "Hit the gym for a workout session. Focus on cardio exercises and strength training.", false);
+        Task.AddTask(task4);
+        Task.PrintTask();
+
+
+
     }
 }
