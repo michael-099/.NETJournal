2b97d3ca1aa6a45304324848136a048eb2036caa
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-11-30 11:07:23 +0300

    Pikachu

--- Pokeman_api/Controllers/PokemonController.cs
+++ Pokeman_api/Controllers/PokemonController.cs
@@ -2,13 +2,28 @@ using Microsoft.AspNetCore.Mvc;
 namespace pokemon_api;
 [ApiController]
 [Route("api/[controller]")]
-public class pokemonController : ControllerBase{
+public class pokemonController : ControllerBase
+{
     private Pokemon Pikachu = new Pokemon();
 
-    [HttpGet]
+    private List<Pokemon> pokemon = new List<Pokemon>
+{
+    new Pokemon(){Id="002",Name="Bulbasaur",Type="Grass",Ability ="Overgrow",Level=3},
+    new Pokemon() {Id="003", Name = "Bulbasaur",Type = "Grass",Ability  = "Overgrow",Level = 3}
+};
 
-    public ActionResult<Pokemon>GetPokemon(){
-        return BadRequest(Pikachu);
-    }
-    }
+
+[HttpGet]
+
+public ActionResult<List<Pokemon>>GetPokemon()
+{
+    return Ok(pokemon);
+}
+[HttpGet("{id}")]
+public ActionResult<Pokemon> GetPokemonById(string id)
+{
+    return Ok(pokemon.Find{pokemon=>pokemon.Id==id });
+}
+
+}
 
