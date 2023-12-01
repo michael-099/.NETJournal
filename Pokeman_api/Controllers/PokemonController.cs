4c1e3a16f9041c1bb08c14be81b5eedd78b1ddc5
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-12-01 14:57:43 +0300

    delete

--- Pokeman_api/Controllers/PokemonController.cs
+++ Pokeman_api/Controllers/PokemonController.cs
@@ -8,8 +8,16 @@ public class pokemonController : ControllerBase
 
     private List<Pokemon> pokemon = new List<Pokemon>
 {
-    new Pokemon() {Id="003", Name = "Bulbasaur",Type = "Grass",Ability  = "Overgrow",Level = 3},
-    new Pokemon() {Id="002", Name = "Bulbasaur",Type = "Grass",Ability  = "Overgrow",Level = 2}
+   new Pokemon { Id = "1", Name = "Bulbasaur", Type = "Grass/Poison", Level = 5 },
+    new Pokemon { Id = "2", Name = "Charmander", Type = "Fire", Level = 5 },
+    new Pokemon { Id = "3", Name = "Squirtle", Type = "Water", Level = 5 },
+    new Pokemon { Id = "5", Name = "Jigglypuff", Type = "Normal/Fairy", Level = 5 },
+    new Pokemon { Id = "6", Name = "Geodude", Type = "Rock/Ground", Level = 5 },
+    new Pokemon { Id = "7", Name = "Abra", Type = "Psychic", Level = 5 },
+    new Pokemon { Id = "8", Name = "Machop", Type = "Fighting", Level = 5 },
+    new Pokemon { Id = "9", Name = "Gastly", Type = "Ghost/Poison", Level = 5 },
+    new Pokemon { Id = "10", Name = "Onix", Type = "Rock/Ground", Level = 5 },
+    // Add more Pokemon as needed
 
 
 };
@@ -36,7 +44,7 @@ public class pokemonController : ControllerBase
     [HttpPost]
     public ActionResult<List<Pokemon>> AddPokemon(Pokemon newPokemon)
     {
-        newPokemon=new Pokemon
+        newPokemon = new Pokemon
         {
             Id = "004",
             Name = "Bulbasaur",
@@ -46,6 +54,18 @@ public class pokemonController : ControllerBase
 
         };
         pokemon.Add(newPokemon);
+        return Ok(pokemon);
+    }
+    [HttpDelete("{id}")]
+
+    public ActionResult<List<Pokemon>> DeletePokemon(string id)
+    {
+        var deletePokemon = pokemon.Find(p => p.Id == id);
+        if (deletePokemon == null)
+        {
+            return NotFound();
+        }
+        pokemon.Remove(deletePokemon);
         return Ok(pokemon);
 
     }
--- Pokeman_api/Models/Pokemon.cs
+++ Pokeman_api/Models/Pokemon.cs
@@ -1,7 +1,7 @@
 namespace pokemon_api;
 public class Pokemon
 {
-    public string Id { get; set; }="001";
+    public string Id { get; set; }="4";
     public string Name { get; set; }="Pikachu ";
     public string Type { get; set; }="";
     public string Ability { get; set; }="";
