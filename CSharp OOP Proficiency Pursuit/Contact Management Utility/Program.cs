c10e5a8e752d65e16a86892f4a370720632418d4
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-12-17 14:18:21 +0300

    blue print of Contact Management Utility

--- CSharp OOP Proficiency Pursuit/Contact Management Utility/Program.cs
+++ CSharp OOP Proficiency Pursuit/Contact Management Utility/Program.cs
@@ -2,31 +2,62 @@
 using System.Collections.Generic;
 namespace Contact_Management;
 
-class Contact{
-    static List<Contact> contact= new  List<Contact>();
-    public string firstName{get;set;}
-    public string lastName{get;set;}
-    public string poneNumber{get;set;}
-    Contact(string firstName,string lastName,int poneNumber){
-        
-    }
+public class Contact
+{
 
-    public void AddContact(string firstName,string lastName,int poneNumber){
-
-
-
-    }
-    public void Display(Contact contact){
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string poneNumber { get; set; }
+    public Contact(string firstName, string lastName, string poneNumber)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.poneNumber = poneNumber;
 
     }
-    public void Search(string firstName){
+}
+public class ContactManager
+{
+    public List<Contact> contacts = new List<Contact>();
+
+    public void AddContact(string firstName, string lastName, string poneNumber)
+    {
+        Contact newcontact = new Contact(firstName, lastName, poneNumber);
+        contacts.Add(newcontact);
+
+    }
+    public void Display()
+    {
+        if (contact.Count > 0)
+        {
+            foreach (Contact contact in contacts)
+            {
+                Console.WriteLine(contact);
+            }
+        }
+
+
+
+    }
+    public void Search(string firstName)
+    {
+        for (int i; i < contacts.Count; i++)
+        {
+            if (contact[i].firstName == firstName)
+            {
+                Console.WriteLine(contacts[i]);
+            }
+        }
+
 
     }
 
 
 }
-class Program{
-    public static void Main(string[] args){
+class Program
+{
+    public static void Main(string[] args)
+    {
 
 
     }
