78c00bf6e9949bf0f7c43a7b8888b296b9eebcff
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-12-17 12:23:48 +0300

    blue print of Contact Management Utility

---
+++ CSharp OOP Proficiency Pursuit/Contact Management Utility/Program.cs
@@ -0,0 +1,28 @@
+﻿using System 
+namespace Contact_Management;
+
+class Contact{
+    static List<Contact> contact= new  List<Contact>();
+    public string firstName{get;set;}
+    public string lastName{get;set;}
+    public int poneNumber{get;set;}
+
+    public void AddContact(string firstName,string lastName,int poneNumber){
+
+
+    }
+    public void Display(Contact contact){
+
+    }
+    public void Search(string firstName){
+
+    }
+
+
+}
+class program{
+    public static void Main(string[] args){
+
+        
+    }
+}
