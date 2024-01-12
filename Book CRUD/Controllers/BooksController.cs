4bdc20503dca26206dbdc900f4e7a445f87bcc99
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-01-12 21:03:01 +0300

    books

---
+++ Book CRUD/Controllers/BooksController.cs
@@ -0,0 +1,60 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Reflection;
+namespace book;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BookController : ControllerBase
+{
+
+
+    private List<Books> books = new List<Books>
+{
+    new Books { Id = "003", Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", Genre = "Classic Fiction" },
+    new Books { Id = "004", Title = "To Kill a Mockingbird", Author = "Harper Lee", Genre = "Classic Fiction" },
+    new Books { Id = "005", Title = "1984", Author = "George Orwell", Genre = "Dystopian Fiction" },
+    new Books { Id = "006", Title = "The Hobbit", Author = "J.R.R. Tolkien", Genre = "Fantasy" },
+    new Books { Id = "007", Title = "The Catcher in the Rye", Author = "J.D. Salinger", Genre = "Coming-of-Age Fiction" },
+    new Books { Id = "008", Title = "Pride and Prejudice", Author = "Jane Austen", Genre = "Classic Romance" },
+    new Books { Id = "009", Title = "The Lord of the Rings", Author = "J.R.R. Tolkien", Genre = "Fantasy" },
+    new Books { Id = "010", Title = "The Harry Potter series", Author = "J.K. Rowling", Genre = "Fantasy" },
+    new Books { Id = "011", Title = "The Chronicles of Narnia", Author = "C.S. Lewis", Genre = "Fantasy" },
+};
+
+
+    [HttpGet("{id}")]
+
+    public ActionResult GetBook(string id)
+    {
+        var theBook = (Books?)null;
+        for (int i = 0; i < books.Count; i++)
+        {
+            if (books[i].Id == id)
+            {
+                theBook = books[i];
+                return Ok(theBook);
+            }
+        }
+
+        if (theBook == null)
+        {
+            return (NotFound());
+
+        }
+        else
+        {
+            return Ok(theBook);
+        }
+
+    }
+    [HttpPost]
+    public IActionResult postbook(string Id ,string Title ,string Author,string Genre){
+        books.Add(new Books { Id = Id, Title = Title, Author = Author, Genre = Genre });
+        return Ok(books);
+    }d
+    }
+
+
+
---
+++ Book CRUD/Models/Books.cs
@@ -0,0 +1,20 @@
+
+using System;
+namespace book;
+
+class Books
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Genre { get; set; }
+
+//    public  Books(string Id ,string Title ,string Author,string Genre){
+//         this.Id=Id;
+//         this.Title=Title;
+//         this.Author=Author;
+//         this.Genre=Genre;
+
+//     }
+
+}
